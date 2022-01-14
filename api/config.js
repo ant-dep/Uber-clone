b4e1604612f61955d3c97f42c26de979e1169d2d
Antoine P <depertat@hotmail.fr>
2022-01-14 14:44:01 +0100

    api connection updated

--- api/config.js
+++ api/config.js
@@ -1,5 +1,5 @@
 export const config = {
-  api_url: "http://localhost:9000",
-  // api_url: "http://uber-and-ubereats-clone.herokuapp.com",
+  // api_url: "http://localhost:9000",
+  api_url: "http://uber-and-ubereats-clone.herokuapp.com",
   pict_url: "http://uber-and-ubereats-clone.herokuapp.com/images/",
 };
